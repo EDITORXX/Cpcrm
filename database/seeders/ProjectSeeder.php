ace7089501b0da6c6e92d9553c011c2d2c7521f3
goldboy99199-ux <goldboy99199@gmail.com>
2026-01-28 11:11:29 +0530

    Fix: Remove description column from ProjectSeeder - use short_overview instead

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -10,9 +10,9 @@ class ProjectSeeder extends Seeder
     public function run(): void
     {
         $projects = [
-            ['name' => 'Sky Wall', 'description' => 'Sky Wall Project', 'is_active' => true],
-            ['name' => 'Eldeco Green', 'description' => 'Eldeco Green Project', 'is_active' => true],
-            ['name' => 'Jashn Elevate', 'description' => 'Jashn Elevate Project', 'is_active' => true],
+            ['name' => 'Sky Wall', 'short_overview' => 'Sky Wall Project', 'is_active' => true],
+            ['name' => 'Eldeco Green', 'short_overview' => 'Eldeco Green Project', 'is_active' => true],
+            ['name' => 'Jashn Elevate', 'short_overview' => 'Jashn Elevate Project', 'is_active' => true],
         ];
 
         foreach ($projects as $project) {
